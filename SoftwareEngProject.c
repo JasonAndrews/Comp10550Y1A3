aae952f2ddac663f757aa92027c6589bfd923bed
Jason Andrews <jasonandrews271192@gmail.com>
2017-04-03 22:15:44 +0100

    Implemented game_definitions.h to separate definitions and function implementation as it makes more sense.

--- SoftwareEngProject.c
+++ SoftwareEngProject.c
@@ -24,6 +24,7 @@
 #include <string.h>
 #include <stdbool.h>
 
+#include "game_definitions.h"
 #include "game_functions.h"
 
 
--- game_functions.h
+++ game_functions.h
@@ -15,70 +15,7 @@
 #ifndef 	GAME_FUNCTIONS_H_
 #define 	GAME_FUNCTIONS_H_
 
-
-#define 	MAX_COLUMN_SIZE		100 // the max column size of each string in the 2D arrays
-
-#define 	EXIT_APPLICATION	'x' // the character required for the main menu loop to break
-
-#define 	MIN_NUM_OF_SLOTS	2
-#define 	MAX_NUM_OF_SLOTS	20
-
-#define 	MAX_PTS				100
-
-#define 	MIN_PLAYERS			2
-#define 	MAX_PLAYERS			6
-
-#define 	TOTAL_SLOT_TYPES	3
-
-
-// values are assigned by default from ranging from 0 to 3 (Elf=0, Human=1, etc.)
-enum PLAYER_TYPES {
-	ELF,
-	HUMAN,
-	OGRE,
-	WIZARD
-};
-
-// The different types available for a slot
-enum SLOT_TYPES {
-	LEVEL_GROUND = 1,
-	HILL = 2,
-	CITY = 3	
-};
-
-/*
-* This struct will be used to store	
-* information about each player's capabilities
-*/
-struct PLAYER_CAPABILITIES {
-	int 
-		smartness,
-		strength,
-		magicSkills,
-		luck,
-		dexterity;
-};
-
-/*
-* This struct will be used to store	
-* information about each player
-*/
-struct PLAYER {
-	char name[20]; // name of the player
-	enum PLAYER_TYPES playerType; // the player type
-	int life_pts; // life points 
-	struct PLAYER_CAPABILITIES caps; // the player's capabilities - see the PLAYER_CAPABILITIES struct above
-	int position; // the player's position in the slot array (zero based as it's an array)
-};
-
-/*
-* This struct will be used to store	
-* information about each slot
-*/
-struct SLOT {
-	struct PLAYER *player;
-	enum SLOT_TYPES slotType;
-};
+#include "game_definitions.h"
 
 // function prototypes
 void setupSlots(unsigned int numPlayers, unsigned int numSlots, struct SLOT *gameSlots);
